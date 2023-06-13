8293943444125da31b6c868d8f6372d3b3681f3d
Sky <skyjin@outlook.com>
2023-06-13 10:50:40 +0800

    141

--- 141.linked-list-cycle.py
+++ 141.linked-list-cycle.py
@@ -13,6 +13,17 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        
+        if not head:
+            return False
+
+        slow_pointer = head
+        fast_pointer = head.next
+        while fast_pointer and fast_pointer.next:
+            if slow_pointer == fast_pointer:
+                return True
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+
+        return False
 # @lc code=end
 

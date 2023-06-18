ed2cf0f5903e8b6f1cfe028aa69fde06ec66f64e
Sky <skyjin@outlook.com>
2023-06-18 19:46:02 +0800

    25

--- 25.reverse-nodes-in-k-group.py
+++ 25.reverse-nodes-in-k-group.py
@@ -12,19 +12,53 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # stk = []
-        last_head
+        # find the pre node of the last head which doesn't have to be reversed
+        pre_of_last_head = ListNode(0, head)
         count = 0
         crt = head
         while crt:
             count += 1
-            if count == 1:
-                last_head = crt
-            elif count == k:
-                last_head = None
+            if count == k:
+                pre_of_last_head = crt
                 count = 0
             
-            crt 
+            crt = crt.next
+        
+        # cut the list
+        remainer = pre_of_last_head.next
+        pre_of_last_head.next = None
+
+        # reverse 
+        dummy = ListNode(0, head)
+        crt = dummy
+        new_head = None
+        pre_tail = None
+        while crt.next:
+            crt = crt.next
+            sub_head = crt
+            count = 1
+            # sub_tail = crt
+            while crt.next and count < k:
+                # reverse
+                # tmp = crt.next
+                # crt.next = crt.next.next
+                # tmp.next = sub_head
+                # sub_head = tmp
+                sub_head, crt.next.next, crt.next = crt.next, sub_head, crt.next.next
+                # count++
+                count += 1
+            if not new_head:
+                new_head = sub_head
+            if pre_tail:
+                pre_tail.next = sub_head
+            pre_tail = crt
+            print(new_head.val)
+            count = 0
+        
+        # append the remainer
+        crt.next = remainer
+
+        return new_head
 
 # @lc code=end
 

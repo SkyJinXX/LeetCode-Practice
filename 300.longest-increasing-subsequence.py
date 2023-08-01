089af9264fe74890a6f70fe790d332da378373ac
Sky <skyjin@outlook.com>
2023-08-01 20:44:07 +0800

    300

--- 300.longest-increasing-subsequence.py
+++ 300.longest-increasing-subsequence.py
@@ -6,6 +6,27 @@
 
 # @lc code=start
 class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int: # O(n*logn)
+        n = len(nums)
+        tails = [nums[0]] # tails[0] means the min tail of a Increasing Subsequence with length 1
+
+        for i in range(1, n):
+            if nums[i] > tails[-1]:
+                tails.append(nums[i])
+            else:
+                L, R = 0, len(tails) - 1
+                while L <= R:
+                    M = L + (R - L) // 2
+                    if tails[M] == nums[i]:
+                        break
+                    elif tails[M] > nums[i]:
+                        R = M - 1
+                    else:
+                        L = M + 1
+                if L > R:
+                    tails[L] = nums[i]
+        
+        return len(tails)
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
         dp = [1] * n

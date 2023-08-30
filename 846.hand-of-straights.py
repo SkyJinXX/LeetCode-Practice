eb41b2168805bfa9a6f6f439b7a2bdd6d7d94d19
Sky <skyjin@outlook.com>
2023-08-29 21:12:06 -0700

    846

--- 846.hand-of-straights.py
+++ 846.hand-of-straights.py
@@ -5,8 +5,28 @@
 #
 
 # @lc code=start
+from collections import Counter
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if groupSize == 1:
+            return True
+        if len(hand) % groupSize != 0:
+            return False
+
+        hand.sort()
+        hand_counter = Counter(hand)
+
+        for val in hand:
+            if val in hand_counter:
+                for val_in_group in range(val, val + groupSize):
+                    if val_in_group not in hand_counter:
+                        return False
+                    else:
+                        hand_counter[val_in_group] -= 1
+                        if hand_counter[val_in_group] == 0:
+                            del hand_counter[val_in_group]
         
+        return True
+
 # @lc code=end
 

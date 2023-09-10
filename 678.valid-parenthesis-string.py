7ba2e69d5df8bd6a4c7619330bff8004070c1b1d
Sky <skyjin@outlook.com>
2023-09-09 22:16:56 -0700

    678

--- 678.valid-parenthesis-string.py
+++ 678.valid-parenthesis-string.py
@@ -7,6 +7,27 @@
 # @lc code=start
 class Solution:
     def checkValidString(self, s: str) -> bool:
+        # check from left to right
+        n_of_left_prth = 0
+        for c in s:
+            if c == '(' or c == '*':
+                n_of_left_prth += 1
+            else:
+                n_of_left_prth -= 1
+            if n_of_left_prth < 0:
+                return False
         
+        # check from left to right
+        n_of_right_prth = 0
+        for i in range(len(s) - 1, -1, -1):
+            c = s[i]
+            if c == ')' or c == '*':
+                n_of_right_prth += 1
+            else:
+                n_of_right_prth -= 1
+            if n_of_right_prth < 0:
+                return False
+        
+        return True
 # @lc code=end
 
